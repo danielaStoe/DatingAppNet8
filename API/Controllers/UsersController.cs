d9da205bca4af9d971ee9bf699f83f3f8b722852
Daniela <stoeckl@qumsult.de>
2024-07-10 19:08:30 +0200

    Angular Working Skeleton

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Immutable;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
 
-namespace API;
+namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UserController
+public class UserController(DataContext context) : ControllerBase
 {
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers(){
+        var users = context.Users.ToList();
+        return Ok(users);
+    }
+
+    [HttpGet("{id:int}")]
+    public ActionResult<AppUser> GetUser(int id){
+        var user = context.Users.Find(id);
+        if(user == null) return NotFound();
+        return user;
+    }
+
 
 }
